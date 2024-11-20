2a89b4730aa0abb86f7eb143ee1ad13b45d59161
Sachini Buddhika <sachiniste3@gmail.com>
2024-11-20 13:53:33 +0530

    cerated the UI of the cart page

--- client/src/components/CartProduct.jsx
+++ client/src/components/CartProduct.jsx
@@ -1,7 +1,85 @@
 import React, { useContext } from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  Typography,
+  IconButton,
+  Container,
+} from "@mui/material";
+import { Remove, Add } from "@mui/icons-material"; // For minus and plus icons
 
-const CartProduct = () => {
-  <div>CartPRoduct</div>;
+const CartProduct = ({ product }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        border: "1px solid",
+        mt: 3,
+        padding: 2,
+        alignItems: "center",
+      }}
+    >
+      <img
+        src={product.image}
+        alt={product.name}
+        style={{
+          width: "100%",
+          maxWidth: "150px",
+          height: "auto",
+          objectFit: "contain",
+        }}
+      />
+      <Box sx={{ marginLeft: { sm: 3 }, mt: { xs: 2, sm: 0 }, flexGrow: 1 }}>
+        <Typography
+          variant="h6"
+          noWrap
+          sx={{
+            maxWidth: "100%",
+            fontSize: { xs: "1rem", sm: "1.25rem" },
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {product.name}
+        </Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            color: "text.secondary",
+            fontSize: { xs: "1rem", sm: "1.25rem" },
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {product.price}
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center", mt: 1 }}>
+          <IconButton sx={{ borderRadius: "50%" }}>
+            <Remove />
+          </IconButton>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "40px",
+              textAlign: "center",
+              minWidth: "40px",
+              fontSize: { xs: "0.875rem", sm: "1rem" },
+            }}
+          >
+            {product.quantity}
+          </Button>
+          <IconButton sx={{ borderRadius: "50%" }}>
+            <Add />
+          </IconButton>
+        </Box>
+        <Button variant="contained" color="warning" size="small" sx={{ mt: 1 }}>
+          Remove
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default CartProduct;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,14 +1,67 @@
 import React, { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
+import { Grid, Box, Typography, Button } from "@mui/material";
+import { useUser } from "../contexts/UserContext.jsx";
+import CartProduct from "../components/CartProduct.jsx";
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
+  const { userInfo } = useUser();
+
+  // Check if user is logged in before showing cart
+  if (!userInfo) {
+    return <p>Please log in to view your cart.</p>;
+  }
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-8">Cart here</div>
-        <div className="col-4"></div>
-      </div>
+    <div>
+      <Box sx={{ padding: 3 }}>
+        <Grid container spacing={3}>
+          {/* The list of added items */}
+          <Grid item xs={12} md={8}>
+            {cart.map((product) => (
+              <Box key={product.id} mb={3}>
+                <CartProduct product={product} />
+              </Box>
+            ))}
+          </Grid>
+
+          {/* Total chekout price */}
+          <Grid item xs={12} md={4}>
+            <Box
+              sx={{
+                backgroundColor: "secondary.main",
+                color: "white",
+                padding: 3,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "space-between",
+                boxSizing: "border-box",
+                width: { xs: "100%", sm: "100%", md: "33%" },
+                marginTop: { xs: 2, sm: 2, md: 0 },
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                Total Items:
+              </Typography>
+              <Typography variant="h5" gutterBottom>
+                Total Price:
+              </Typography>
+              <Button
+                variant="contained"
+                color="warning"
+                sx={{
+                  width: "100%",
+                  marginTop: 2,
+                }}
+              >
+                Checkout
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 };
